aac86378e2c64fff5c9a6a1d03c88a96bee59dc9
naharansh <naharransh123@gmail.com>
2026-06-19 11:54:22 +0530

    feat: initialize and export Prisma client instance with global caching

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const connectionString = process.env.DATABASE_URL!;
-
-const url = new URL(connectionString);
-url.searchParams.set("connectionLimit", "5");
-url.searchParams.set("acquireTimeout", "30000");
-url.searchParams.set("connectTimeout", "15000");
-
-const adapter = new PrismaMariaDb(url.toString());
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -16,6 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter });
+  new PrismaClient();
 
 globalForPrisma.prisma = prisma;
